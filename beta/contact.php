c8a1cdb4ab13dd66aaa3a7f6ee40d740ef3da4e6
Lionel Jeronimo <lionoweb@gmail.com>
2015-01-06 15:26:25 +0100

    1.1
    Modif Line

--- beta/contact.php
+++ beta/contact.php
@@ -80,7 +80,7 @@
 
     <form class="col-md-6 col-md-offset-3 col-sm-12 container" id="spec_contact" action="contact.php" method="get">
 
-        <div colspan="2" class="header_propose">Une envie de nous contacter ?</div>
+        <div colspan="2" class="header_propose blueback">Une envie de nous contacter ?</div>
 
         <div class="greyback">
 
