8d9dcc02b7dc47920a6004c354be2cf412100837
Lionel Jeronimo <lionoweb@gmail.com>
2015-01-22 21:14:55 +0100

    Mysql 1.2

--- beta/inc/mysql.php
+++ beta/inc/mysql.php
@@ -1,7 +1,7 @@
 <?php 
 if(preg_match("/localhost|127\.0\.0\.1/", $_SERVER['HTTP_HOST'])) {
 	//FOR MAMP
-	$mysql = mysqli_connect(":/Applications/MAMP/tmp/mysql/mysql.sock","root","root","swappyfraa0") or die("Error MySQL Connect..."); 
+	$mysql = mysqli_connect("localhost","root","root","swappyfraa0") or die("Error MySQL Connect..."); 
 } else {
 	$mysql = mysqli_connect("swappyfraa0.mysql.db","swappyfraa0","2Dside77","swappyfraa0") or die("Error " . mysqli_error($mysql)); 
 }
