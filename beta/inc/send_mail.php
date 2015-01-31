63ff1532ebe792e899e6197c04e3bd334597a290
Lionel Jeronimo <lionoweb@gmail.com>
2015-01-31 09:18:19 +0100

    Bug Github

--- beta/inc/send_mail.php
+++ beta/inc/send_mail.php
@@ -3,7 +3,7 @@ require_once("mysql.php");
 require_once("mail.php");
 $mail = new mailer();
 $arr = array();
-if(isset($_POST['mail'])) {
+if(isset($_POST['email'])) {
 	$arr = $mail->send_contact_form($_POST);
 }
 echo json_encode($arr);
